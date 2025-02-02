c64d0f7897021e1b8e7d93575d10e1902f1d1603
n4cr4 <n4cr4.dev@gmail.com>
2025-02-02 22:09:36 +0900

    fix: typo

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/original/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/original/keymap.c
@@ -74,7 +74,7 @@ void oledkit_render_info_user(void) {
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(2, KC_ENT):
-        case SFT_T(KC_LANG2):
+        case SFT_T(KC_LNG2):
             return true;
         default:
             return false;
